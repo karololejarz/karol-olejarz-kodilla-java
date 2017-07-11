18626ba832847aff096dbb11e187d4c1f563963b
karololejarz <karol1olejarz@gmail.com>
2017-07-11 18:55:58 +0200

    Moduł 6. Wprowadzenie do testowania oprogramowania

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -0,0 +1,10 @@
+package com.kodilla.testing;
+
+/**
+ * Created by Karol Olejarz on 11.07.2017.
+ */
+public class TestingMain {
+    public static void main(String[] args){
+        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    }
+}
