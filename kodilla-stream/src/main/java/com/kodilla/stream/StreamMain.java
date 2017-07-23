314efee9305d5a11ffd7a8249a8513632ce2c5b4
karololejarz <karol1olejarz@gmail.com>
2017-07-23 13:00:59 +0200

    Moduł 7. Stream

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -14,30 +14,18 @@ public class StreamMain {
         Forum forum = new Forum();
         /*Make sure there is toString in ForumUser!!!*/
 
-        String forumMales = forum.getUserList().stream()
-                .filter(forumUser -> forumUser.getUserGender() == 'M')
-                .map(ForumUser::toString)
-                .collect(Collectors.joining(",\n","<<",">>"));
-        System.out.println("Males on forum are: " + forumMales);
-
         LocalDate currentDate = LocalDate.of(2017,07,19);
 
-        String forumOldies = forum.getUserList().stream()
-                .filter(forumUser -> Period.between(forumUser.getUserDateOfBirth(), currentDate).getYears() > 20)
-                .map(ForumUser::toString)
-                .collect(Collectors.joining(",\n","<<",">>"));
-        System.out.println("Older than 20 years on forum: " + forumOldies);
-
-        String forumActive = forum.getUserList().stream()
+        Map<Integer,ForumUser> forumMapFiltered = forum.getUserList().stream()
+                .filter(forumUser -> forumUser.getUserGender() == 'M')
+                .filter(forumUser -> Period.between(forumUser.getUserDateOfBirth(), currentDate).getYears() >= 20)
                 .filter(forumUser -> forumUser.getUserPostCount() > 0)
-                .map(ForumUser::toString)
-                .collect(Collectors.joining(",\n","<<",">>"));
-        System.out.println("Active on forum: " + forumActive);
-
-        Map<Integer,ForumUser> forumMap = forum.getUserList().stream()
                 .collect(Collectors.toMap(ForumUser::getUserId, forumUser -> forumUser));
-        System.out.println("ForumMap:" + "\n" + forumMap);
 
+        System.out.println("Men aged at least 20 who are active on forum: ");
+        forumMapFiltered.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
         /*7.3. end*/
     }
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/forum/Forum.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/forum/Forum.java
@@ -8,8 +8,10 @@ public final class Forum {
     private final List<ForumUser> userList = new ArrayList<>();
 
     public Forum() {
-        userList.add(new ForumUser(1, "User1", 'M', LocalDate.of(1990,06,07), 0));
-        userList.add(new ForumUser(2, "User2", 'F', LocalDate.of(2000,06,07), 1));
+        userList.add(new ForumUser(1, "Man1", 'M', LocalDate.of(1990,06,07), 0));
+        userList.add(new ForumUser(2, "Woman1", 'F', LocalDate.of(2000,06,07), 1));
+        userList.add(new ForumUser(3, "Man2", 'M', LocalDate.of(1980,06,07), 1));
+        userList.add(new ForumUser(4, "Man3", 'M', LocalDate.of(1980,06,07), 1));
     }
 
     public List<ForumUser> getUserList() {
