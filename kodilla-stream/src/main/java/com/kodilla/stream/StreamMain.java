e826ee4754241e15cb084363489638470ca6b6de
karololejarz <karol1olejarz@gmail.com>
2017-07-24 11:28:49 +0200

    Moduł 7. Stream

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,7 +1,13 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.forum.Forum;
 import com.kodilla.stream.forum.ForumUser;
+import com.kodilla.stream.lambda.ExecuteSaySomething;
+import com.kodilla.stream.lambda.Executor;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.reference.FunctionalCalculator;
 
 import java.time.LocalDate;
 import java.time.Period;
@@ -10,6 +16,14 @@ import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
+        /*7.1. start*/
+        PoemBeautifier beautifier = new PoemBeautifier();
+        beautifier.beautify("Example", text -> "ABC"+text+"ABC");
+        beautifier.beautify("Example", text -> text.toUpperCase());
+        beautifier.beautify("Example", text -> text+text.hashCode());
+        beautifier.beautify("Example", text -> text+ " Length: " + text.length());
+        /*7.1. end*/
+
         /*7.3. start*/
         Forum forum = new Forum();
         /*Make sure there is toString in ForumUser!!!*/
