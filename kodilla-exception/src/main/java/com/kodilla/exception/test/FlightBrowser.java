9f1804f85967fa6d8fdc17b67d56ec5dc88a2326
karololejarz <karol1olejarz@gmail.com>
2017-07-31 10:51:26 +0200

    Moduł 8. Obsługa wyjątków

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightBrowser.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightBrowser.java
@@ -5,28 +5,25 @@ import java.util.HashMap;
 
 public class FlightBrowser {
 
-    HashMap<String, Boolean> flightsMap = new HashMap<>();
+    private HashMap<String, Boolean> flightsMap = new HashMap<>();
 
-    public void createFlight (Flight flight) {
+    protected void createFlight (Flight flight) {
         flightsMap.put(flight.getDepartureAirport(), true);
     }
 
-    public HashMap<String, Boolean> getFlightsMap() {
+    protected HashMap<String, Boolean> getFlightsMap() {
         return flightsMap;
     }
 
-    public void findFlight(String departureAirport) throws RouteNotFoundException {
+    protected boolean findFlight(String departureAirport) throws RouteNotFoundException {
 
         if(flightsMap.keySet().contains(departureAirport)) {
             System.out.println("There is a flight to " + departureAirport);
+            return true;
         }
-
         else {
-            try {
-                throw new RouteNotFoundException();
-            } catch (RouteNotFoundException r) {
-                System.out.println("There is no flight to " + departureAirport);
-            }
+            System.out.println("There is no flight to " + departureAirport);
+            throw new RouteNotFoundException();
         }
     }
 
--- kodilla-exception/src/test/java/com/kodilla/exception/test/FlightBrowserTest.java
+++ kodilla-exception/src/test/java/com/kodilla/exception/test/FlightBrowserTest.java
@@ -2,7 +2,7 @@ package com.kodilla.exception.test;
 
 public class FlightBrowserTest {
 
-    public static void main(String[] args) throws RouteNotFoundException {
+    public static void main(String[] args) {
         FlightBrowser browser = new FlightBrowser();
         Flight rze = new Flight("RZE");
         Flight krk = new Flight("KRK");
@@ -10,7 +10,24 @@ public class FlightBrowserTest {
         browser.createFlight(krk);
         System.out.println(browser);
 
-        browser.findFlight("RZE");
-        browser.findFlight("WAR");
+        try {
+            browser.findFlight("RZE");
+        }
+        catch (RouteNotFoundException e)         {
+            System.out.println("There is an exception");
+        }
+        finally {
+            System.out.println("Search ended");
+        }
+
+        try {
+            browser.findFlight("WAR");
+        }
+        catch (RouteNotFoundException e) {
+            System.out.println("This is an exception");
+        }
+        finally {
+            System.out.println("Search ended");
+        }
     }
 }
