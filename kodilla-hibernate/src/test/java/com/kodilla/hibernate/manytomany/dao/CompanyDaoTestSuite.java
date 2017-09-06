d4ffcbd6caaf225deac689eafe9f9e2047600988
karololejarz <karol1olejarz@gmail.com>
2017-09-06 16:24:52 +0200

    Moduł 13. Hibernate

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -18,6 +18,7 @@ public class CompanyDaoTestSuite {
     @Autowired
     EmployeeDao employeeDao;
 
+    /*
     public void createAndPopulateTables()
     {
         Employee johnSmith = new Employee("John", "Smith");
@@ -41,12 +42,10 @@ public class CompanyDaoTestSuite {
         lindaKovalsky.getCompanies().add(greyMatter);
 
         companyDao.save(softwareMachine);
-        int softwareMachineId = softwareMachine.getId();
         companyDao.save(dataMaesters);
-        int dataMaestersId = dataMaesters.getId();
         companyDao.save(greyMatter);
-        int greyMatterId = greyMatter.getId();
     }
+    */
 
     @Test
     public void testSaveManyToMany() {
@@ -91,36 +90,121 @@ public class CompanyDaoTestSuite {
             companyDao.delete(greyMatterId);
         } catch (Exception e) {
         }
-
     }
 
     @Test
     public void testFindEmployeeByLastName(){
-        createAndPopulateTables();
+        /*createAndPopulateTables();*/
 
-        String lastname = "Smith";
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+        Company softwareMachine = new Company("Software Machine");
+        Company dataMaesters = new Company("Data Maesters");
+        Company greyMatter = new Company("Grey Matter");
+
+        softwareMachine.getEmployees().add(johnSmith);
+        dataMaesters.getEmployees().add(stephanieClarckson);
+        dataMaesters.getEmployees().add(lindaKovalsky);
+        greyMatter.getEmployees().add(johnSmith);
+        greyMatter.getEmployees().add(lindaKovalsky);
+
+        johnSmith.getCompanies().add(softwareMachine);
+        johnSmith.getCompanies().add(greyMatter);
+        stephanieClarckson.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(greyMatter);
+
+        companyDao.save(softwareMachine);
+        companyDao.save(dataMaesters);
+        companyDao.save(greyMatter);
+
+        String lastname = "Clarckson";
         int found = employeeDao.retrieveLastName(lastname).size();
         Assert.assertEquals(1,found);
 
         try {
-            companyDao.deleteAll();
-            employeeDao.deleteAll();
+            companyDao.delete(softwareMachine);
+            companyDao.delete(dataMaesters);
+            companyDao.delete(greyMatter);
+            employeeDao.delete(johnSmith);
+            employeeDao.delete(stephanieClarckson);
+            employeeDao.delete(lindaKovalsky);
         } catch (Exception e) {
         }
+
+        /*try {
+            //3 companies
+            companyDao.delete(0);
+            companyDao.delete(1);
+            companyDao.delete(2);
+            //3 workers, but 5 company-posts
+            employeeDao.delete(0);
+            employeeDao.delete(1);
+            employeeDao.delete(2);
+            employeeDao.delete(3);
+            employeeDao.delete(4);
+        } catch (Exception e) {
+        }
+        */
     }
 
     @Test
     public void testFindCompanyByFirst3Chars(){
-        createAndPopulateTables();
+        /*createAndPopulateTables();*/
+
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+        Company softwareMachine = new Company("Software Machine");
+        Company dataMaesters = new Company("Data Maesters");
+        Company greyMatter = new Company("Grey Matter");
+
+        softwareMachine.getEmployees().add(johnSmith);
+        dataMaesters.getEmployees().add(stephanieClarckson);
+        dataMaesters.getEmployees().add(lindaKovalsky);
+        greyMatter.getEmployees().add(johnSmith);
+        greyMatter.getEmployees().add(lindaKovalsky);
+
+        johnSmith.getCompanies().add(softwareMachine);
+        johnSmith.getCompanies().add(greyMatter);
+        stephanieClarckson.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(greyMatter);
+
+        companyDao.save(softwareMachine);
+        companyDao.save(dataMaesters);
+        companyDao.save(greyMatter);
 
         String first3Chars = "Sof";
         int found = companyDao.retrieve3FirstChars(first3Chars).size();
         Assert.assertEquals(1,found);
 
         try {
-            companyDao.deleteAll();
-            employeeDao.deleteAll();
+            companyDao.delete(softwareMachine);
+            companyDao.delete(dataMaesters);
+            companyDao.delete(greyMatter);
+            employeeDao.delete(johnSmith);
+            employeeDao.delete(stephanieClarckson);
+            employeeDao.delete(lindaKovalsky);
         } catch (Exception e) {
         }
+
+        /*try {
+            //3 companies
+            companyDao.delete(0);
+            companyDao.delete(1);
+            companyDao.delete(2);
+            //3 workers, but 5 company-posts
+            employeeDao.delete(0);
+            employeeDao.delete(1);
+            employeeDao.delete(2);
+            employeeDao.delete(3);
+            employeeDao.delete(4);
+        } catch (Exception e) {
+        }
+        */
     }
 }
